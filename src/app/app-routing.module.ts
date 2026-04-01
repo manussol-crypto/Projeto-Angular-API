dc31f17cdb3c377ca0f902bd244ec6c404717aa7
Manu Carvalho <manussol@gmail.com>
2026-04-01 10:00:11 -0300

    Add App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
+import { CadastroProdutoComponent } from './componentes/cadastroproduto/cadastroproduto.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'painel-principal', component: PainelPrincipalComponent },
+  { path: '', redirectTo: '/painel-principal', pathMatch: 'full' },
+  { path: 'cadastro-produto', component: CadastroProdutoComponent }, // Cadastro de novo produto
+  { path: 'cadastro-produto/:id', component: CadastroProdutoComponent }, // Edição de produto
+  { path: '', redirectTo: '/painel-principal', pathMatch: 'full' } //
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
+import { CadastroProdutoComponent } from './componentes/cadastroproduto/cadastroproduto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    PainelPrincipalComponent,
+    CadastroProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
